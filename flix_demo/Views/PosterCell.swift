184d1c8b0049f66858c3a0660c9f72f346d1303e
Kaila Prochaska <kailaprochaska@174.dhcp.apogeetelecom.com>
2018-09-17 21:12:45 -0500

    commiting changes to flix

---
+++ flix_demo/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_demo
+//
+//  Created by Kaila Prochaska on 9/11/18.
+//  Copyright © 2018 Kaila Prochaska. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
